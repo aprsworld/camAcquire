066c8be413438825dadf244c888f1e33d6823bdb
James Jarvis <jj@aprsworld.com>
2019-09-27 11:55:07 +0000

    Scripts now run in order. Added overlay example. pzPower watchdog

--- actions.post/010-cam.aprsworld.com.sh
+++ actions.post/010-cam.aprsworld.com.sh
@@ -2,6 +2,18 @@
 
 # replace usname:password with your cam.aprsworld.com username and password
 
-curl -F file=@$3 http://username:password@cam.aprsworld.com/up/
+curl -F file=@$3 http://A5575:pond@cam.aprsworld.com/up/
+retVal=$?
+
+if [ $retVal -eq 0 ]; then
+	# reset write watchdog if curl returned 0
+	echo "# resetting write watchdog since curl exited error free"
+	/home/aprs/aprsI2C/aprs/pzPowerI2C --reset-write-watchdog
+else
+	echo "# curl returned $retVal which is an error. Not resetting write watchdog"
+fi
+
+exit $retVal
+
 
 
